f62fabfe97535e17ace43306f2babc77eaf4068c
Philip Osborne <pdfosborne@gmail.com>
2025-06-06 10:50:31 +0100

    Engine - Define Opponent Agents within engine file

--- environment/engine.py
+++ environment/engine.py
@@ -4,8 +4,11 @@ from chess import Board
 import numpy as np
 
 # Opponent agent imports
+from elsciRL.agents.agent_abstract import Agent
 from elsciRL.agents.random_agent import RandomAgent
-from opponent_agents.random_filtered_agent import RandomAgentFiltered
+import urllib
+import json
+import random
 
 # Imports for rendering
 import chess.svg
@@ -13,6 +16,38 @@ from io import BytesIO
 from PIL import Image
 import matplotlib.pyplot as plt
 
+
+# Engine cannot use relative imports if called by elsciRL GUI so all functions must be defined here
+class RandomAgentFiltered(Agent):
+    """ This is simply a random decision maker, does not learn. 
+        Possible actions filtered on human player data to minimise randomness."""
+    def __init__(self):
+        # --- Data Import -------------------------------------------------------------------------------------------------
+        # dict[req_board][move_uci] = {"prev_moves_uci": list,
+        #                              "whiteWon": int, "blackWon": int, "draw": int,
+        #                              "totalGames": int } 
+
+        # Get player move stats data from public source
+        data_source = 'https://raw.githubusercontent.com/pdfosborne/elsciRL-App-Chess/main/environment/opponent_agents/data/stats_map.json'
+        self.player_data_dict = json.loads(urllib.request.urlopen(data_source).read())
+
+        # with open('./environment/opponent_agents/data/stats_map.json', 'r') as json_file:
+        #     self.player_data_dict = json.load(json_file)
+
+    def policy(self, board_p: Board):
+        # Go through each known move from player data and extract move_uci of known moves
+        filtered_legal_moves = []
+        for move in self.player_data_dict[str(board_p.fen())]:
+            if self.player_data_dict[str(board_p.fen())][move]["totalGames"] > 10:
+                filtered_legal_moves.append(move)
+        
+        # In cases where a known list of moves doesn't exist, pick from full list of legal moves
+        if len(filtered_legal_moves) != 0:
+            move_uci = str(random.choice(filtered_legal_moves))
+        else:
+            move_uci = str(random.choice(list(board_p.legal_moves))) 
+            
+        return move_uci
 
 class Engine:
     """Defines the environment function from the generator engine.
